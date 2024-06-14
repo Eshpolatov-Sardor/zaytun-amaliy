807e0d903da0be6fedb46fdc46f7f52fa59590c7
Eshpolatov-Sardor <167778318+Eshpolatov-Sardor@users.noreply.github.com>
2024-06-15 00:33:30 +0500

    sdfghj

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-//sardor bilan dilshod yozadi
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      margin: 5,
+      loop: true,
+    });
+  });
+  
